4f2275f77e52a7c54892a078edd170417d030504
7138gmhampton <0407435@abertay.ac.uk>
2020-11-26 20:20:54 +0000

    Remove corpse code

--- src/detector_node/detector_node.ino
+++ src/detector_node/detector_node.ino
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <pins_arduino.h>
-// #include <RH_ASK.h>
 
 #include "flame_sensor.h"
 #include "transceiver.h"
@@ -11,8 +10,6 @@
 #define PACKET_LENGTH 32
 
 FlameSensor* flame_sensor;
-// RH_ASK radio(2000, RECEIVER_PIN, TRANSMITTER_PIN);
-// const char *notification = "Fire detected!";
 MeshNetwork::Transceiver* radio;
 
 void notifyOfFire()
@@ -31,8 +28,6 @@ void setup()
     Serial.begin(115200);
     
     flame_sensor = new FlameSensor(FLAME_SENSOR_PIN);
-    // if (!driver)
-    // if (!radio.init()) Serial.println("Radio failed to initialise");
     radio = new MeshNetwork::Transceiver(RECEIVER_PIN, TRANSMITTER_PIN, PACKET_LENGTH);
     if (!radio->initialiseRadio()) Serial.println("Radio failed to initialise");
     
@@ -41,39 +36,6 @@ void setup()
 
 void loop()
 {
-    // if (flame_sensor.isDetectingFire()) {
-    //     Serial.println("Fire!");
-    //     // const char *notification = "Fire detected!";
-    //     bool success = radio.send((uint8_t*)notification, strlen(notification));
-    //     radio.waitPacketSent();
-    //     const char* user_notification = success ? "Transmitted" : "Not transmitted";
-    //     Serial.println(user_notification);
-    // }
-    // else Serial.println("All clear...");
-
-    // bool success = radio.send((uint8_t*)notification, strlen(notification));
-    // radio.waitPacketSent();
-    // Serial.println(success);
-
-    // uint8_t buffer[strlen(notification)];
-    // uint8_t buffer_length = sizeof(buffer);
-    // if (radio.recv(buffer, &buffer_length)) {
-    //     Serial.print("Received: ");
-    //     Serial.println((char*)buffer);
-    // }
-
-    // Serial.print("Max msg length for this driver ");
-    // Serial.println(radio.maxMessageLength());
-
-    // Serial.println(analogRead(A0));
-
-    // delay(500);
-
-    // uint8_t receipt_buffer[PACKET_LENGTH];
-    // uint8_t buffer_length = sizeof(receipt_buffer);
-    // if (radio.recv(receipt_buffer, &buffer_length)) {
-
-    // }
     uint8_t receipt_buffer[PACKET_LENGTH];
     uint8_t buffer_length = sizeof(receipt_buffer);
     if (radio->receiveData(receipt_buffer)) {
