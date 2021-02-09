cd55d4ab38db707ed4af621e58d93ffa451008a3
7138gmhampton <0407435@abertay.ac.uk>
2021-02-09 16:23:35 +0000

    Implement test 'hello, world'

---
+++ src/test/dummy_test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+TEST(dummy, whenTrueThenTrue)
+{
+    bool dummy = true;
+
+    ASSERT_TRUE(dummy);
+}
+
+TEST(dummy, whenFalseThenFalse)
+{
+    bool dummy = true;
+
+    ASSERT_TRUE(dummy);
+}
