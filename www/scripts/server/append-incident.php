0766b3cd15dd7d07901ba3fac9640e699270fae3
7138gmhampton <0407435@abertay.ac.uk>
2021-02-24 19:46:18 +0000

    Change append script to use digital ocean server

--- www/scripts/server/append-incident.php
+++ www/scripts/server/append-incident.php
@@ -10,15 +10,19 @@ error_reporting(E_ALL);
 //     fclose($incident_list);
 // }
 
-$server = 'lochnagar.abertay.ac.uk';
-$database = 'sql0407435';
-$password = 'PAnQq1jJKwkk';
+// $server = 'lochnagar.abertay.ac.uk';
+// $database = 'sql0407435';
+// $password = 'PAnQq1jJKwkk';
+$server = 'localhost';
+$user = 'beaumains';
+$database = 'mf';
+$password = '1`~.LmdSMfy&/A';
 
 if (isset($_GET['incident'])) {
-    $connection = new mysqli($server, $database, $password, $database);
+    $connection = new mysqli($server, $user, $password, $database);
     if ($connection->connect_error) die('Connection failed: '.$connection->connect_error);
 
-    $sql_command = 'INSERT INTO mf_incident (code) VALUES ('.$_GET['incident'].')';
+    $sql_command = 'INSERT INTO incident (code) VALUES ('.$_GET['incident'].')';
 
     $connection->query($sql_command);
 
