1489c13dd47b7029423cfd1690217c42105bbffe
7138gmhampton <0407435@abertay.ac.uk>
2021-02-24 19:39:47 +0000

    Update portal home page to utilise digital ocean server

--- www/index.php
+++ www/index.php
@@ -5,20 +5,27 @@
         <div>
             <ul>
                 <?php
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
                 // $incidents = file('data/incidents.txt');
 
                 // foreach ($incidents as $incident)
                 //     echo '<li>'.$incident.'</li>';
 
-                $server = 'lochnagar.abertay.ac.uk';
-                $database = 'sql0407435';
-                $password = 'PAnQq1jJKwkk';
+                // $server = 'lochnagar.abertay.ac.uk';
+                // $database = 'sql0407435';
+                // $password = 'PAnQq1jJKwkk';
+                $server = 'localhost';
+                $user = 'beaumains';
+                $database = 'mf';
+                $password = '1`~.LmdSMfy&/A';
 
-                $connection = new mysqli($server, $database, $password, $database);
+                $connection = new mysqli($server, $user, $password, $database);
                 if ($connection->connect_error) die('Connection failed: '.$connection->connect_error);
 
                 // $sql_command = 'INSERT INTO mf_incident (code) VALUES ('.$_GET['incident'].')';
-                $sql_command = 'SELECT code FROM mf_incident';
+                $sql_command = 'SELECT code FROM incident';
 
                 $response = $connection->query($sql_command);
                 
