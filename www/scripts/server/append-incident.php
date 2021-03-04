a811872d8fc6a5f146f48d02b7f79369538a980c
7138gmhampton <0407435@abertay.ac.uk>
2021-03-04 16:18:00 +0000

    Apply MVC to incident append script

--- www/scripts/server/append-incident.php
+++ www/scripts/server/append-incident.php
@@ -3,19 +3,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$server = 'localhost';
-$user = 'beaumains';
-$database = 'mf';
-$password = '1`~.LmdSMfy&/A';
+require_once('/var/www/html/mesh-fire/scripts/server/controller/incident_controller.php');
 
+// $server = 'localhost';
+// $user = 'beaumains';
+// $database = 'mf';
+// $password = '1`~.LmdSMfy&/A';
+
+// if (isset($_GET['incident'])) {
+//     $connection = new mysqli($server, $user, $password, $database);
+//     if ($connection->connect_error) die('Connection failed: '.$connection->connect_error);
+
+//     $sql_command = 'INSERT INTO incident (code) VALUES ('.$_GET['incident'].')';
+
+//     $connection->query($sql_command);
+
+//     $connection->close();
+// }
 if (isset($_GET['incident'])) {
-    $connection = new mysqli($server, $user, $password, $database);
-    if ($connection->connect_error) die('Connection failed: '.$connection->connect_error);
-
-    $sql_command = 'INSERT INTO incident (code) VALUES ('.$_GET['incident'].')';
-
-    $connection->query($sql_command);
-
-    $connection->close();
+    IncidentController::create($_GET['incident']);
 }
 ?>
