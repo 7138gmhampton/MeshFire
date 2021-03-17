5cf975891b54a4acc243c7340af4a66087d4e730
7138gmhampton <0407435@abertay.ac.uk>
2021-03-17 20:52:08 +0000

    Attempt at implementing dummy message transmission

--- src/detector_node/detector_node.ino
+++ src/detector_node/detector_node.ino
@@ -3,6 +3,9 @@
 // #include <WiFiManager.h>
 #include <WiFiManager.h>
 #include <ESP8266HTTPClient.h>
+
+#define FREQUENCY_868
+
 #include <LoRa_E32.h>
 
 #include "flame_sensor.h"
@@ -13,12 +16,15 @@
 #define RECEIVER_PIN D8
 #define PACKET_LENGTH 32
 
+// #define FREQUENCY_868
+
 WiFiManager wifi_manager;
 FlameSensor* flame_sensor;
 MeshNetwork::Transceiver* radio;
 LoRa_E32 lora_transceiver(D2, D3);
 
 volatile bool notifying = false;
+volatile bool dummy_send = false;
 
 void notifyOfIncident()
 {
@@ -46,9 +52,10 @@ ICACHE_RAM_ATTR void fireDetect()
 
 ICACHE_RAM_ATTR void sendDummyRadio()
 {
-    ResponseStatus response_status = lora_transceiver.sendMessage("Check...1...2...");
-    Serial.println("Button press message:");
-    Serial.println(response_status.getResponseDescription());
+    // ResponseStatus response_status = lora_transceiver.sendMessage("Check...1...2...");
+    // Serial.println("Button press message:");
+    // Serial.println(response_status.getResponseDescription());
+    dummy_send = true;
 }
 
 void setup()
@@ -57,6 +64,8 @@ void setup()
 
     // Connect to WiFi
     wifi_manager.setConfigPortalTimeout(180);
+    // WiFi.macAddress()
+    // wifi_manager.setHostname()
     wifi_manager.autoConnect();
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
@@ -66,9 +75,22 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(FLAME_SENSOR_PIN), fireDetect, FALLING);
 
     lora_transceiver.begin();
+
+    ResponseStructContainer config_container = lora_transceiver.getConfiguration();
+    Configuration lora_config = *(Configuration*)config_container.data;
+    Serial.print("Channel - ");
+    Serial.println(lora_config.getChannelDescription());
+    // lora_config.CHAN = FREQUENCY_868;
+    lora_config.CHAN = 0x06;
+    Serial.print("Channel Set - ");
+    Serial.println(lora_config.getChannelDescription());
+    lora_transceiver.setConfiguration(lora_config);
+    int dummy = OPERATING_FREQUENCY;
+
     ResponseStatus response_status = lora_transceiver.sendMessage("Hello, World?");
     Serial.println(response_status.getResponseDescription());
-    attachInterrupt(digitalPinToInterrupt(D7), sendDummyRadio, RISING);
+    pinMode(D7, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(D7), sendDummyRadio, FALLING);
 }
 
 void loop()
@@ -77,6 +99,15 @@ void loop()
     
     // Serial.print(flame_sensor->isDetectingFire());
     // delay(1000);
+
+    // Serial.println(digitalRead(D7));
+
+    if (dummy_send) {
+        ResponseStatus response_status = lora_transceiver.sendMessage("Check...1...2...");
+        Serial.println("Button press message:");
+        Serial.println(response_status.getResponseDescription());
+        dummy_send = false;
+    }
 
     if (lora_transceiver.available() > 1) {
         ResponseContainer response_container = lora_transceiver.receiveMessage();
