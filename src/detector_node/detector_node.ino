609e10d35f8e7edc65fff4b7d4cd60533206e124
7138gmhampton <0407435@abertay.ac.uk>
2021-04-27 19:02:57 +0100

    Remove corpse code

--- src/detector_node/detector_node.ino
+++ src/detector_node/detector_node.ino
@@ -15,33 +15,14 @@ EventLog* event_log;
 Dispatcher* dispatcher;
 WifiPortal* web;
 
-volatile bool sensor_blanked = false;
 unsigned long blanking_start;
-
-// ICACHE_RAM_ATTR void fireDetect();
-
-// ICACHE_RAM_ATTR void reinitialiseSensor()
-// {
-//     // attachInterrupt(FLAME_SENSOR_PIN, fireDetect, FALLING);
-//     sensor_blanked = false;
-// }
 
 ICACHE_RAM_ATTR void fireDetect()
 {
     if ((millis() - blanking_start > 30000) && flame_sensor->isDetectingFire()) {
         event_log->addEvent(FlameSensor::generateEvent());
-        // detachInterrupt(FLAME_SENSOR_PIN);
-        // sensor_blanked = true;
-        // timer1_attachInterrupt(reinitialiseSensor);
-        // timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
-        // timer1_write(6000000);
         blanking_start = millis();
     }
-}
-
-ICACHE_RAM_ATTR void sendDummyRadio()
-{
-    event_log->addEvent(FlameSensor::generateEvent());
 }
 
 void setup()
@@ -56,8 +37,6 @@ void setup()
     dispatcher = new Dispatcher(radio, web);
     flame_sensor =  new FlameSensor(FLAME_SENSOR_PIN);
 
-    // pinMode(D1, INPUT_PULLUP);
-    // attachInterrupt(digitalPinToInterrupt(D1), sendDummyRadio, FALLING);
     attachInterrupt(digitalPinToInterrupt(FLAME_SENSOR_PIN), fireDetect, FALLING);
 }
 
