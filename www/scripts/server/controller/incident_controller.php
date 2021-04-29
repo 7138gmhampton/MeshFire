902637e642f8e3f381efec7dcafb6deea2ad01ab
7138gmhampton <0407435@abertay.ac.uk>
2021-04-29 16:23:15 +0100

    Update incident controller create to include mac address

--- www/scripts/server/controller/incident_controller.php
+++ www/scripts/server/controller/incident_controller.php
@@ -3,9 +3,9 @@ require_once('/var/www/html/mesh-fire/scripts/server/model/incident_model.php');
 
 class IncidentController
 {
-    public static function create(string $code)
+    public static function create(string $code, string $mac_address)
     {
-        $incident = array('code' => $code);
+        $incident = array('code' => $code, 'mac_address' => $mac_address);
 
         IncidentModel::createIncident(json_encode($incident));
     }
