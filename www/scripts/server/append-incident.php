99e4e7548f30c8be0306ec7f5d06e9afd21ee7dd
7138gmhampton <0407435@abertay.ac.uk>
2021-04-29 16:24:41 +0100

    Update append script to include mac address

--- www/scripts/server/append-incident.php
+++ www/scripts/server/append-incident.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 
 require_once('/var/www/html/mesh-fire/scripts/server/controller/incident_controller.php');
 
-if (isset($_POST['incident'])) {
-    IncidentController::create($_POST['incident']);
+if (isset($_POST['incident']) && isset($_POST['mac_address'])) {
+    IncidentController::create($_POST['incident'], $_POST['mac_address']);
 }
 else http_response_code(400);
 ?>
