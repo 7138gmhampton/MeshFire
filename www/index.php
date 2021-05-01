daa349a8195f19f01c1e6af5bafc7d5eda1fb424
7138gmhampton <0407435@abertay.ac.uk>
2021-05-01 19:46:31 +0100

    Apply bootstrap to reporting portal

--- www/index.php
+++ www/index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
     <body>
         <h1>MeshFire Reporting Portal</h1>
         <div>
