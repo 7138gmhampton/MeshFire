10f09ec969fd23fc76070f04443f20041155f17a
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 18:09:35 +0100

    Create outline of full log page

---
+++ www/full_log.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>MeshFire Full Log</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="general.css" />
+    </head>
+    <body>
+        <div class="container-fluid text-center mf-banner pb-3">
+            <h1>MeshFire Reporting Portal</h1>
+        </div>
+        <div class="container text-center">
+            <a href="#" class="btn mf-button">Back to Main</a>
+        </div>
+    </body>
+</html>
