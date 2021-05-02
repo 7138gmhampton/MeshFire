349225fc123a152b0de969d529290761eb348a21
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 19:29:21 +0100

    Add model method to get some events

--- www/scripts/server/model/incident_model.php
+++ www/scripts/server/model/incident_model.php
@@ -31,13 +31,37 @@ class IncidentModel extends Database
     public static function getRecent(int $number)
     {
         $command = 'SELECT code, mac_address, time_stamp FROM incident '
-            .'ORDER BY time_stamp ASC '
-            .'LIMIT :number';
+            .'ORDER BY time_stamp DESC '
+            .'LIMIT :number;';
         $result = null;
 
         try {
             $statement = self::prepareStatement($command, self::$down_user, self::$down_password);
             $statement->bindParam(':number', $number, PDO::PARAM_INT);
+
+            $statement->execute();
+
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $err) {
+            echo 'Error: '.$err->getMessage();
+            return null;
+        }
+
+        return json_encode($result);
+    }
+
+    public static function getSome(int $start, int $number)
+    {
+        $command = 'SELECT code, mac_address, time_stamp FROM incident '
+            .'ORDER BY time_stamp DESC '
+            .'LIMIT :offset, :number;';
+            $result = null;
+
+        try {
+            $statement = self::prepareStatement($command, self::$down_user, self::$down_password);
+            $statement->bindParam(':number', $number, PDO::PARAM_INT);
+            $statement->bindParam(':offset', $start, PDO::PARAM_INT);
 
             $statement->execute();
 
