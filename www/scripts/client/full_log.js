eb557468e529a34ebb3a50ec2d7e1e440d6f95f2
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 19:20:11 +0100

    Add cuntionality to capture scrolling to the bottom

--- www/scripts/client/full_log.js
+++ www/scripts/client/full_log.js
@@ -1,3 +1,19 @@
-window.onscroll = function () {
-    console.log('Scrolling...');
+var next_row = 0;
+var last_position = 0;
+
+function drawMore()
+{
+    console.log('Draw More...')
+}
+
+window.onscroll = function () 
+{
+    // console.log('Scrolling...');
+    let bottom = document.body.clientHeight - window.innerHeight
+    // console.log(window.scrollY + ' => ' + bottom);
+    // console.log()
+    let position = window.scrollY;
+
+    if (position > last_position && position >= bottom) drawMore()
+    last_position = position;
 }
