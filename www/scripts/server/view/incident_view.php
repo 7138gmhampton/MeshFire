ae96437906196d6e12b1de839d1d1327dc290591
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 20:49:33 +0100

    Reduce repetition via static factory for assoc array constructor

--- www/scripts/server/view/incident_view.php
+++ www/scripts/server/view/incident_view.php
@@ -7,11 +7,24 @@ class Incident
     private $mac_address;
     private $time_stamp;
 
-    function __construct(string $code, string $mac_address, string $time_stamp)
+    // function __construct(string $code, string $mac_address, string $time_stamp)
+    // {
+    //     $this->code = $code;
+    //     $this->mac_address = $mac_address;
+    //     $this->time_stamp = date_create($time_stamp);
+    // }
+
+    private function __construct() { }
+
+    public static function buildFromArray($assoc_array)
     {
-        $this->code = $code;
-        $this->mac_address = $mac_address;
-        $this->time_stamp = date_create($time_stamp);
+        $incident = new Incident();
+
+        $incident->code = $assoc_array['code'];
+        $incident->mac_address = $assoc_array['mac_address'];
+        $incident->time_stamp = date_create($assoc_array['time_stamp']);
+
+        return $incident;
     }
 
     function printTableRow()
@@ -35,11 +48,12 @@ class IncidentView
 
         $all_incidents = array();
         foreach ($incidents as $each_incident) {
-            $next_incident = new Incident(
-                $each_incident['code'], 
-                $each_incident['mac_address'], 
-                $each_incident['time_stamp']);
-            array_push($all_incidents, $next_incident);
+            // $next_incident = new Incident(
+            //     $each_incident['code'], 
+            //     $each_incident['mac_address'], 
+            //     $each_incident['time_stamp']);
+            // array_push($all_incidents, $next_incident);
+            array_push($all_incidents, Incident::buildFromArray($each_incident));
         }
 
         return $all_incidents;
@@ -51,11 +65,12 @@ class IncidentView
 
         $these_incidents = array();
         foreach ($incidents as $incident) {
-            $next_incident = new Incident(
-                $incident['code'], 
-                $incident['mac_address'], 
-                $incident['time_stamp']);
-            array_push($these_incidents, $next_incident);
+            // $next_incident = new Incident(
+            //     $incident['code'], 
+            //     $incident['mac_address'], 
+            //     $incident['time_stamp']);
+            // array_push($these_incidents, $next_incident);
+            array_push($these_incidents, Incident::buildFromArray($incident));
         }
 
         return $these_incidents;
@@ -67,11 +82,12 @@ class IncidentView
 
         $recent_incidents = array();
         foreach ($incidents as $each_incident) {
-            $next_incident = new Incident(
-                $each_incident['code'], 
-                $each_incident['mac_address'], 
-                $each_incident['time_stamp']);
-            array_push($recent_incidents, $next_incident);
+            // $next_incident = new Incident(
+            //     $each_incident['code'], 
+            //     $each_incident['mac_address'], 
+            //     $each_incident['time_stamp']);
+            // array_push($recent_incidents, $next_incident);
+            array_push($recent_incidents, Incident::buildFromArray($each_incident));
         }
 
         return $recent_incidents;
