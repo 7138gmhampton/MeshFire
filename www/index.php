0d4d270cf2a651345d239721965f258eb59fb3d7
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 14:33:43 +0100

    Set page header in fluid container

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <h1>MeshFire Reporting Portal</h1>
+        <!-- <h1>MeshFire Reporting Portal</h1> -->
         <!-- <div>
             <table>
                 <thead>
@@ -29,6 +29,9 @@
                 ?>
             </table>
         </div> -->
+        <div class="container-fluid text-center">
+            <h1>MeshFire Reporting Portal</h1>
+        </div>
         <div class="container">
             <!-- <div class="row"> -->
                 <!-- <div class="col"> -->
