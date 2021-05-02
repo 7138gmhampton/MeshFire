b5db4d20ed00745f3e09d92a57cb8b99bd705f2d
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 20:05:08 +0100

    Implement AJAX request

--- www/scripts/client/full_log.js
+++ www/scripts/client/full_log.js
@@ -1,9 +1,24 @@
 var next_row = 0;
 var last_position = 0;
+const GULP_SIZE = 20
+var awaiting = false;
 
 function drawMore()
 {
     console.log('Draw More...')
+    awaiting = true;
+    let xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function ()
+    {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            awaiting = false;
+        }
+    };
+
+    xhttp.open('GET', 'http://139.59.173.54/mesh-fire/scripts/server/get-some-incidents.php?start=' + next_row + '&number=' + 20);
+    xhttp.send();
 }
 
 window.onscroll = function () 
@@ -14,6 +29,6 @@ window.onscroll = function ()
     // console.log()
     let position = window.scrollY;
 
-    if (position > last_position && position >= bottom) drawMore()
+    if (position > last_position && position >= bottom & !awaiting) drawMore()
     last_position = position;
 }
