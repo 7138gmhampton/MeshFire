81bb2a8145d3b042a45389f58fbe6c65e67d2dd9
7138gmhampton <0407435@abertay.ac.uk>
2021-05-02 19:49:12 +0100

    Create php script to return some events for an AJAX request

---
+++ www/scripts/server/get-some-incidents.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// require_once('/var/www/html/mesh-fire/scripts/server/view/incident_view.php');
+require_once('/var/www/html/mesh-fire/scripts/server/model/incident_model.php');
+
+if (isset($_GET['start']) && isset($_GET['number']))
+    echo IncidentModel::getSome($_GET['start'], $_GET['number']);
+else http_response_code(400);
+?>
